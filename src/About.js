197ff0703a08b7946afcb5d8f831848ab4791aab
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-07-19 16:20:54 -0700

    about and footer page complete

--- src/About.js
+++ src/About.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <main>
-    <h1>About</h1>
+    <main className='About'>
+    <h2>About</h2>
+    <p style={{ marginTop: "1rem"}}>
+      This blog was created by FePaiva, to share experience about living in a foreign country.
+    </p>
     </main>
   )
 }
